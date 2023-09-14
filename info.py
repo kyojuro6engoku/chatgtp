8592f7ddca3e4cbfa006653971aae0e2b7432c41
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-09-14 21:06:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,16 @@
 import re
 from os import environ
 
-# Bot information
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-OPENAI_API_KEY = environ['OPENAI_API_KEY']
+# This file contains configuration information for your Telegram bot and ChatGPT.
+
+# Telegram Bot API Token
+TELEGRAM_BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+# OpenAI API Key
+OPENAI_API_KEY = "YOUR_OPENAI_API_KEY"
+
+# ChatGPT Engine Name
+CHATGPT_ENGINE = "text-davinci-002"  # You can adjust this based on your chosen engine
+
+# Maximum Tokens for ChatGPT Response
+MAX_TOKENS = 50  # Adjust based on your desired response length
