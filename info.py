fa76e14d160ed3e4842d32f1776818d09ede2786
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-09-14 20:39:53 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,8 @@
+import re
+from os import environ
+
+# Bot information
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+OPENAI_API_KEY = environ['OPENAI_API_KEY']
