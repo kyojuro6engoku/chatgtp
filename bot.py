81f8ae698ce8e9df857bccd2df642bfb7ddeece8
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-09-16 19:40:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,62 +1,17 @@
 import os
-import telegram
-import openai
-from telegram.ext import Updater, MessageHandler, filters, CommandHandler, token
-
-# Set your Telegram Bot API token and OpenAI API key as environment variables
-TELEGRAM_BOT_TOKEN = os.environ.get("6094342214:AAEYviwCeUXV_I-zExdtsk0AFeKgepfRNyo")
-OPENAI_API_KEY = os.environ.get("sk-XKZ39L027DDzMy h6qyoaT3BlbkFJWWJVWiOnLaCkQhLzNc7J")
-
-
-# Initialize the Telegram Bot
-import telegram
-
-bot = telegram.Bot(token='6094342214:AAEYviwCeUXV_I-zExdtsk0AFeKgepfRNyo')
-
-# Initialize the OPENAI KEY
-ai_key = OPENAI_API_KEY
-
-# Define a function to handle incoming Telegram messages
-def handle_message(update, context):
-    user_id = update.message.chat_id
-    user_message = update.message.text
-    
-    # Send the user's message to ChatGPT
-    response = send_to_chatgpt(user_message)
-    
-    # Send the ChatGPT response back to the user
-    context.bot.send_message(chat_id=user_id, text=response)
-
-# Define a function to interact with ChatGPT
-def send_to_chatgpt(user_message):
-    # Customize the prompt to ChatGPT as needed
-    prompt = f"User: {user_message}\nAI:"
-    
-    # Send the user's message to ChatGPT
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # Choose the appropriate ChatGPT engine
-        prompt=prompt,
-        max_tokens=50  # Adjust based on your desired response length
-    )
-    
-    return response.choices[0].text.strip()
-
-TELEGRAM_BOT_TOKEN = 'your_bot_token_here'
-# Define your message handler function
-def handle_message(update, context):
-    # Your message handling logic here
-    pass
-
-TELEGRAM_BOT_TOKEN = 'your_bot_token_here'
-
-# Create an Updater instance
-updater = Updater(token='your_bot_token_here')
-
-# Get the dispatcher from the updater
-dispatcher = updater.dispatcher
-
-# Add a message handler
-dispatcher.add_handler(MessageHandler(filters.text & ~Filters.command, handle_message))
-
-# Start the bot
-updater.start_polling()
+import telebot
+from transformers import pipeline
+# Initialize the ChatGPT model
+model = pipeline("text-generation", model="EleutherAI/finetuned-gpt-neo-1.3B")
+# Initialize the Telegram bot
+bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_TOKEN"))
+# Define the function to handle incoming messages
+def handle_message(message):
+    # Get the message text
+    text = message.text
+    # Generate a response using the ChatGPT model
+    response = model(text, max_length=1024)
+    # Send the response to the user
+    bot.send_message(message.chat.id, response["generated_text"])
+# Start the Telegram bot
+bot.polling()
